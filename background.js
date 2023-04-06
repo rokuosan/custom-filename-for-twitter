3a34d2c746175da7a2afcaa653098b312fd7c655
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-06 22:10:34 +0900

    保存するファイル名を変更可能にした

--- background.js
+++ background.js
@@ -18,31 +18,19 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
     let ppt = /\/\d{1,}\//g;
     let postId = info.linkUrl.match(ppt)[0].replaceAll('/', '')
 
-    console.log(postId)
-
-    let filename = `Twitter@${userId}_${postId}.jpg`
+    const timestamp = new Date().toISOString().replace(/[-:.]/g, '');
 
     const imageUrl = info.srcUrl;
-    chrome.downloads.download({
-      url: imageUrl,
-      filename: filename,
-      saveAs: false,
-    });
-    // getFilename(function (filename) {
-    //   chrome.downloads.download({
-    //     url: imageUrl,
-    //     filename: filename,
-    //     saveAs: false,
-    //   });
-    // });
+
+    chrome.storage.local.get(['filename']).then((d) => {
+      console.log(d)
+      const fn = d.filename.replaceAll('{screen_id}', userId).replaceAll('{post_id}', postId).replaceAll('{current_time}', timestamp)
+
+      chrome.downloads.download({
+        url: imageUrl,
+        filename: `${fn}.jpg`,
+        saveAs: false,
+      });
+    })
   }
 });
-
-function getFilename(callback) {
-  chrome.storage.sync.get('filename', function (data) {
-    const filename = data.filename || 'image';
-    const timestamp = new Date().toISOString().replace(/[-:.]/g, '');
-    const fullFilename = `${filename}_${timestamp}.jpg`;
-    callback(fullFilename);
-  });
-}
--- resources/js/options.js
+++ resources/js/options.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   const filenameInput = document.querySelector('#filename-input');
-  const saveButton = document.querySelector('#save-button');
+  const saveButton = document.querySelector('#filename-save');
 
   // 保存されたファイル名がある場合、入力欄にその値を設定する
-  chrome.storage.sync.get('filename', function (data) {
-    filenameInput.value = data.filename || 'image';
+  chrome.storage.local.get('filename', function (data) {
+    filenameInput.value = data.filename || 'Twitter_@{screen_id}_{post_id}';
   });
 
   // 保存ボタンが押されたら、ファイル名を保存する
   saveButton.addEventListener('click', function () {
-    const filename = filenameInput.value || 'image';
-    chrome.storage.sync.set({ filename: filename });
+    const filename = filenameInput.value || 'Twitter_@{screen_id}_{post_id}';
+    chrome.storage.local.set({ filename: filename });
   });
 });
